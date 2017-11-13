fa87dbd2782424a71798965c6d01440726d13f9b
Nicolai Davies <nicolaidavies.nd@gmail.com>
2017-11-13 11:04:42 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div v-for="item in items" :style="{'background-color': 'blue'}">
+      {{item.name}}
+    </div>
+    <auto-input @select="add_item"></auto-input>
+  </div>
+</template>
+
+<script>
+  import AutoInput from './AutoInput.vue'
+
+  export default {
+    name: 'app',
+    components: {AutoInput},
+    data() {
+      return {
+        items: []
+      }
+    },
+    methods: {
+      add_item(item) {
+        this.items.push(item)
+      }
+    }
+  }
+</script>
+
+<style>
+  ul {
+    list-style-type: none;
+    padding-left: 0;
+  }
+
+  li {
+    padding: 0.25em 0;
+  }
+</style>
---
+++ src/AutoInput.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <input type="text" v-model="text" />
+    <ul>
+      <li v-for="result in results" @click="select_item(result)">
+        {{result.name}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: 'app',
+    data() {
+      return {
+        text: '',
+      }
+    },
+    computed: {
+      results() {
+        if (!this.text.length) return []
+        return this.options.filter(a => a.name.toLocaleLowerCase().indexOf(this.text.toLocaleLowerCase()) !== -1)
+      },
+      options() {
+        return [
+          {name: "Filter name"},
+          {name: "Filter date"},
+          {name: "Filter id"},
+          {name: "Filter type"},
+          {name: "Filter description"}
+        ]
+      }
+    },
+    methods: {
+      select_item(item) {
+        this.$emit('select', item)
+      }
+    }
+  }
+</script>
+
+<style>
+  ul {
+    list-style-type: none;
+    padding-left: 0;
+  }
+
+  li {
+    padding: 0.25em 0;
+  }
+</style>
